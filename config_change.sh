33de59e207ad94ab10172a6d4de624c04e80189d
xlsama <im.xlsama@gmail.com>
2022-10-04 10:03:49 +0800

    feat: add config_change.sh

---
+++ config_change.sh
@@ -0,0 +1,7 @@
+# vscode settings.json glboal-code.snippets
+cp ~/Library/Application\ Support/Code/User/settings.json ./VSCode/.vscode/
+cp ~/Library/Application\ Support/Code/User/snippets/global.code-snippets ./VSCode/.vscode/
+# alacritty.yml 
+cp ~/.config/alacritty/alacritty.yml ./Alacritty/ 
+# tmux.conf
+cp ~/.config/tmux/tmux.conf  ./tmux/
