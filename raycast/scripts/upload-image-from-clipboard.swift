2a7e14895893118fe8b9aa86f16282b0bf725aee
xlsama <im.xlsama@gmail.com>
2022-11-29 22:52:49 +0800

    chore: no message

---
+++ raycast/scripts/upload-image-from-clipboard.swift
@@ -0,0 +1,17 @@
+#!/usr/bin/swift
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Upload image from clipboard
+// @raycast.mode silent
+
+// Optional parameters:
+// @raycast.icon images/smms.png
+// @raycast.packageName Productivity
+
+// Documentation:
+// @raycast.description Upload image from clipboard to SM.MS
+// @raycast.author xlsama
+// @raycast.authorURL https://github.com/xlsama
+
+print("Hello World!")
