a5fe6d663de5bc3c1f4feba2b997587f2abc51c7
xlsama <im.xlsama@gmail.com>
2023-01-08 11:01:21 +0800

    chore: update

---
+++ raycast/scripts/bilibili.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Bilibili
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/bilibili.ico
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open Bilibili in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://t.bilibili.com/"
