fe22bfba27d4a56f16ee0a204f725ffc45c08756
xlsama <im.xlsama@gmail.com>
2023-01-10 23:40:20 +0800

    chore: update

---
+++ raycast/scripts/notion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title notion
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://www.notion.so/images/favicon.ico
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open notion in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://www.notion.so/"
