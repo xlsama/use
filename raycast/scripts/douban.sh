0bb473a5b742f31987168c5de55aa62594140fc8
xlsama <im.xlsama@gmail.com>
2023-04-06 14:29:27 +0800

    chore: add douban scripts

---
+++ raycast/scripts/douban.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Douban
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://img1.doubanio.com/favicon.ico
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open 豆瓣 in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://book.douban.com/people/xlsama/do"
+
