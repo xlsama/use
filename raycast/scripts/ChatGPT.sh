5334bafa8bbaae74f9c4f61dbabb6c6b6839fd57
xlsama <im.xlsama@gmail.com>
2023-06-06 00:04:24 +0800

    chore: update raycast scripts

---
+++ raycast/scripts/ChatGPT.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ChatGPT
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/chatgpt.png
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open ChatGPT in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://chat.openai.com/?model=gpt-4-browsing"
