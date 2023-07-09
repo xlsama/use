7f7f49510d50d7cff1670830aed992508f847485
xlsama <im.xlsama@gmail.com>
2023-07-09 10:29:31 +0800

    chore(apps): update

---
+++ raycast/scripts/spotify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Spotify
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://open.spotifycdn.com/cdn/images/favicon.0f31d2ea.ico
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open Spotify in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://open.spotify.com/"
---
+++ raycast/scripts/ticktick.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title TickTick
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon https://cncdn.dida365.com/static/img/favicon.ico
+# @raycast.packageName Link
+
+# Documentation:
+# @raycast.description Open TickTick in Google Chrome
+# @raycast.author xlsama
+# @raycast.authorURL https://github.com/xlsama
+
+open "https://dida365.com/"
