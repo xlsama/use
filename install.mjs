c92594ba240aba9007cd9c94c93e7275b9a6f18e
xlsama <im.xlsama@gmail.com>
2024-04-10 09:19:26 +0800

    chore: update

--- install.mjs
+++ install.mjs
@@ -1,67 +1,62 @@
 #!/usr/bin/env zx
 
-const HOME_DIR = os.homedir()
-
-const link_map = [
+const LINK_MAP = [
   // config
   {
-    source: `${HOME_DIR}/code/use/config/fish`,
-    target: `${HOME_DIR}/.config/fish`,
+    source: '~/code/use/config/fish',
+    target: `~/.config/fish`,
   },
   {
-    source: `${HOME_DIR}/code/use/config/nvim`,
-    target: `${HOME_DIR}/.config/nvim`,
+    source: `~/code/use/config/nvim`,
+    target: `~/.config/nvim`,
   },
   {
-    source: `${HOME_DIR}/code/use/config/starship.toml`,
-    target: `${HOME_DIR}/.config/starship.toml`,
+    source: `~/code/use/config/starship.toml`,
+    target: `~/.config/starship.toml`,
   },
   // git
   {
-    source: `${HOME_DIR}/code/use/git/.gitconfig`,
-    target: `${HOME_DIR}/.gitconfig`,
+    source: `~/code/use/git/.gitconfig`,
+    target: `~/.gitconfig`,
   },
   {
-    source: `${HOME_DIR}/code/use/git/.gitconfig_work`,
-    target: `${HOME_DIR}/.gitconfig_work`,
+    source: `~/code/use/git/.gitconfig_work`,
+    target: `~/.gitconfig_work`,
   },
   {
-    source: `${HOME_DIR}/code/use/git/.gitignore`,
-    target: `${HOME_DIR}/.gitignore`,
+    source: `~/code/use/git/.gitignore`,
+    target: `~/.gitignore`,
   },
   // vscode
   {
-    source: `${HOME_DIR}/code/use/vscode/settings.json`,
-    target: `${HOME_DIR}/Library/Application Support/Code/User/settings.json`,
+    source: `~/code/use/vscode/settings.json`,
+    target: `~/Library/Application Support/Code/User/settings.json`,
   },
   {
-    source: `${HOME_DIR}/code/use/vscode/keybindings.json`,
-    target: `${HOME_DIR}/Library/Application Support/Code/User/keybindings.json`,
+    source: `~/code/use/vscode/keybindings.json`,
+    target: `~/Library/Application Support/Code/User/keybindings.json`,
   },
   {
-    source: `${HOME_DIR}/code/use/vscode/global.code-snippets`,
-    target: `${HOME_DIR}/Library/Application Support/Code/User/snippets/global.code-snippets`,
+    source: `~/code/use/vscode/global.code-snippets`,
+    target: `~/Library/Application Support/Code/User/snippets/global.code-snippets`,
   },
 ]
 
-const global_npm_pkg_list = ['@antfu/ni', 'vite']
+console.log()
+print('link config files...')
 
-function print(msg) {
-  console.log(chalk.magenta(msg))
-  console.log(
-    chalk.white(
-      '---------------------------------------------------------------------------------------------------------------------------',
-    ),
-  )
-}
+const HOME_DIR = os.homedir()
 
-print('link config files')
-link_map.forEach(async ({ source, target }) => {
-  await $`rm -rf ${target}`
-  await $`ln -s -F ${source} ${target}`
-})
+await Promise.all(
+  LINK_MAP.map(async ({ source, target }) => {
+    source = source.replace('~', HOME_DIR)
+    target = target.replace('~', HOME_DIR)
+    await $`rm -rf ${target}`
+    await $`ln -s -F ${source} ${target}`
+  }),
+)
 
-print('set macOS system settings')
+print('set macOS system settings...')
 // finder
 await $`defaults write NSGlobalDomain AppleShowAllExtensions -bool true`
 await $`defaults write com.apple.finder ShowPathbar -bool true`
@@ -69,16 +64,30 @@ await $`defaults write com.apple.finder _FXSortFoldersFirst -bool true`
 // keyboard
 await $`defaults write ApplePressAndHoldEnabled -bool false`
 
-print('install vscode extensions')
-const { recommendations } = await fs.readJson('./vscode/extensions.json')
-recommendations.forEach(async name => {
-  await $`code --install-extension ${name}`
-})
+print('install npm packages...')
 
-print('install npm packages')
+const GLOBAL_NPM_PKG_LIST = ['vite', '@antfu/ni', 'pnpm']
+
 await $`npm set registry https://registry.npmjs.org/`
-global_npm_pkg_list.forEach(async name => {
-  await $`npm i -g ${name}`
-})
+await Promise.all(
+  GLOBAL_NPM_PKG_LIST.map(async name => {
+    await $`npm i -g ${name}`
+  }),
+)
+
+print('install vscode extensions...')
+
+const { recommendations } = await fs.readJson('./vscode/extensions.json')
+
+await Promise.all(
+  recommendations.map(async name => {
+    await $`code --install-extension ${name}`
+  }),
+)
 
 await $`cp ./.hooks/pre-commit ./.git/hooks/`
+
+function print(msg) {
+  console.log(chalk.magenta(msg))
+  console.log()
+}
