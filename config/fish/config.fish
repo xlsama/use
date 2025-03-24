4fec40612c6e3d82541217ec665aa8c170fb40fa
xlsama <im.xlsama@gmail.com>
2025-03-24 16:14:02 +0800

    chore: clarify alias for node version manager in fish configuration and remove conda initialization block

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -62,7 +62,7 @@ alias l='ls -1A'
 alias cat='bat'
 alias v='nvim'
 alias o='open'
-alias nv='fnm' # node version
+alias nv='fnm' # node version manager
 alias python='python3'
 alias reload='exec fish'
 
@@ -97,16 +97,3 @@ function gclw
     code ~/w/$project_name
     exit
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/anaconda3/bin/conda
-    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /opt/homebrew/anaconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
