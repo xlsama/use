e29f42df2f2e9ad4f90661444555b40d8cad96d6
xlsama <im.xlsama@gmail.com>
2025-08-17 01:34:55 +0800

    chore: update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -106,7 +106,7 @@ alias up='nlx taze -I -r'
 alias venv='source .venv/bin/activate.fish'
 
 alias claude="/Users/xlsama/.claude/local/claude"
-alias ai='claude --dangerously-skip-permissions'
+alias ai='codex'
 
 function c
     code $argv
@@ -129,8 +129,3 @@ function gclw
     c ~/w/$project_name
     exit
 end
-
-# claude code
-set -gx ANTHROPIC_AUTH_TOKEN sk-075aPaOCXZIXjdAyoXxHsYr6D5VK5idH9shknsXliG8tOtvC
-set -gx ANTHROPIC_API_KEY sk-075aPaOCXZIXjdAyoXxHsYr6D5VK5idH9shknsXliG8tOtvC
-set -gx ANTHROPIC_BASE_URL https://code.ppchat.vip
