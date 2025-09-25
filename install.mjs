7b455b051dae468b2f66302e15b609958750afff
Li Xiaolong <im.xlsama@gmail.com>
2025-09-25 16:05:10 +0800

    Update install.mjs

--- install.mjs
+++ install.mjs
@@ -87,7 +87,7 @@ const { recommendations } = await fs.readJson("./vscode/extensions.json");
 
 await Promise.all(
 	recommendations.map(async (name) => {
-		await $`code --install-extension ${name}`;
+		await $`code --install-extension -f ${name}`;
 	}),
 );
 
