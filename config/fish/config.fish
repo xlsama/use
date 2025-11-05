b4e9501594a7d57d6679134159a3bf824ac13b3b
xlsama <im.xlsama@gmail.com>
2025-11-05 11:13:56 +0800

    chore: add docker config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -140,3 +140,7 @@ function gclw
     c ~/w/$project_name
     exit
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
